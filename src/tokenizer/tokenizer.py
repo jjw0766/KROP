de2d3abb4b6573b9e6d1fe02fb9384635f5c22e4
jwengr <jinwoo0766@gmail.com>
2025-08-08 15:32:52 +0900

    Add dataset generation notebooks and tokenizer implementation
    - Created `generate_kor_union_dataset.ipynb` for generating the Kor-Union dataset with noise.
    - Created `generate_m_absa_dataset.ipynb` for generating the M-ABSA dataset with noise methods.
    - Implemented `HangulTokenizer` and `SentenceTokenizer` classes in `tokenizer.py` for handling Korean text tokenization, including jamo and character encoding.

---
+++ src/tokenizer/tokenizer.py
@@ -0,0 +1,362 @@
+import re
+import torch
+
+from collections import deque, Counter
+from jamotools import split_syllables, join_jamos
+from transformers import AutoTokenizer
+
+class HangulTokenizer:    
+    def __init__(self, base_tokenizer_name):
+        self.base_tokenizer_name = base_tokenizer_name
+        self.base_tokenizer = AutoTokenizer.from_pretrained(self.base_tokenizer_name)
+        self.base_tokenizer.pad_token_id = 128
+        self.base_tokenizer.pad_token = self.base_tokenizer.decode([self.base_tokenizer.pad_token_id])
+        self.space_token_id = self.base_tokenizer.encode(' ', add_special_tokens=False)[-1]
+        char_start, char_end = 0xAC00, 0xD7A3  # 가-힣
+        self.kor_chars = list(set([chr(code) for code in range(char_start, char_end + 1)]))
+        self.char_3ids = []
+        self.char_1ids = []
+        for kor_char in self.kor_chars:
+            ids = self.base_tokenizer.encode(kor_char, add_special_tokens=False)
+            if len(ids)==3:
+                self.char_3ids.append(ids)
+            else:
+                ids = ids+2*[self.base_tokenizer.pad_token_id]
+                self.char_1ids.append(ids)
+        self.chos = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+        self.joongs = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+        self.jongs = [self.base_tokenizer.pad_token, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+        jamos = list(set(self.chos) | set(self.joongs) | set(self.jongs))
+        jamo_ids = self.base_tokenizer(jamos, add_special_tokens=False)['input_ids']
+        self.jamo_to_id = {jamo: jamo_id[-1] for jamo, jamo_id in zip(jamos, jamo_ids)}
+        self.cho_ids = [self.jamo_to_id[cho] for cho in self.chos]
+        self.joong_ids = [self.jamo_to_id[joong] for joong in self.joongs]
+        self.jong_ids = [self.jamo_to_id[jong] for jong in self.jongs]
+        self.id_to_jamo = {jamo_id: jamo for jamo, jamo_id in self.jamo_to_id.items()}
+
+    def encode_jamo(self, sentence):
+        encoded_ids = []
+        token_type_ids = []
+        past_chars = ''
+        for char in sentence:
+            if char in self.kor_chars:
+                if past_chars:
+                    past_chars_encoded = self.base_tokenizer.encode(past_chars, add_special_tokens=False)
+                    encoded_ids.extend(past_chars_encoded)
+                    token_type_ids.extend([0]*len(past_chars_encoded))
+                past_chars=''
+                char_splitted = list(split_syllables(char))[:3]
+                char_splitted = char_splitted + (3-len(char_splitted))*[self.base_tokenizer.pad_token]
+                cho, joong, jong = char_splitted
+                encoded_ids.extend([self.jamo_to_id[cho], self.jamo_to_id[joong], self.jamo_to_id[jong]])
+                token_type_ids.extend([1,2,3])
+            else:
+                past_chars = past_chars+char
+        if past_chars:
+            past_chars_encoded = self.base_tokenizer.encode(past_chars, add_special_tokens=False)
+            encoded_ids.extend(past_chars_encoded)
+            token_type_ids.extend([0]*len(past_chars_encoded))
+        return encoded_ids, token_type_ids
+
+    def decode_jamo(self, encoded_ids, token_type_ids):
+        encoded_ids = deque(encoded_ids)
+        token_type_ids = deque(token_type_ids)
+        decoded = []
+        past_ids = []
+        while len(encoded_ids):
+            encoded_id = encoded_ids.popleft()
+            token_type_id = token_type_ids.popleft()
+            if token_type_id==0:
+                past_ids.append(encoded_id)
+            else:
+                decoded.append(self.base_tokenizer.decode(past_ids))
+                past_ids = []
+                cho_id = encoded_id
+                joong_id = encoded_ids.popleft()
+                jong_id = encoded_ids.popleft()
+                token_type_ids.popleft()
+                token_type_ids.popleft()
+                char = join_jamos([self.id_to_jamo[cho_id], self.id_to_jamo[joong_id], self.id_to_jamo[jong_id]])[:1]
+                decoded.append(char)
+        decoded.append(self.base_tokenizer.decode(past_ids))
+        return ''.join(decoded)
+
+    def encode_char(self, sentence):
+        encoded_ids = []
+        token_type_ids = []
+        past_chars = ''
+        for char in sentence:
+            if char in self.kor_chars:
+                if past_chars:
+                    past_chars_encoded = self.base_tokenizer.encode(past_chars, add_special_tokens=False)
+                    encoded_ids.extend(past_chars_encoded)
+                    token_type_ids.extend([0]*len(past_chars_encoded))
+                past_chars=''
+                encoded_id = self.base_tokenizer.encode(char, add_special_tokens=False)
+                encoded_id = encoded_id + (3-len(encoded_id)) * [self.base_tokenizer.pad_token_id]
+                encoded_ids.extend(encoded_id)
+                token_type_ids.extend([4,4,4])
+            else:
+                past_chars = past_chars+char
+        if past_chars:
+            past_chars_encoded = self.base_tokenizer.encode(past_chars, add_special_tokens=False)
+            encoded_ids.extend(past_chars_encoded)
+            token_type_ids.extend([0]*len(past_chars_encoded))
+        return encoded_ids, token_type_ids
+
+    def decode_char(self, encoded_ids, token_type_ids):
+        encoded_ids = deque(encoded_ids)
+        token_type_ids = deque(token_type_ids)
+        decoded = []
+        past_ids = []
+        while len(encoded_ids):
+            encoded_id = encoded_ids.popleft()
+            token_type_id = token_type_ids.popleft()
+            if token_type_id==0:
+                past_ids.append(encoded_id)
+            else:
+                decoded.append(self.base_tokenizer.decode(past_ids))
+                past_ids = []
+                id1 = encoded_id
+                id2 = encoded_ids.popleft()
+                id3 = encoded_ids.popleft()
+                token_type_ids.popleft()
+                token_type_ids.popleft()
+                [id1, id2, id3]
+                char = self.base_tokenizer.decode([id1, id2, id3])[:1]
+                decoded.append(char)
+        decoded.append(self.base_tokenizer.decode(past_ids))
+        return ''.join(decoded)
+
+    def encode_jamo_from_char_encoded(self, encoded_ids, token_type_ids):
+        encoded_ids = deque(encoded_ids)
+        token_type_ids = deque(token_type_ids)
+        encoded_ids_new = []
+        token_type_ids_new = []
+        while len(encoded_ids):
+            encoded_id = encoded_ids.popleft()
+            token_type_id = token_type_ids.popleft()
+            if token_type_id==0:
+                encoded_ids_new.append(encoded_id)
+                token_type_ids_new.append(token_type_id)
+            else:
+                encoded_id2 = encoded_ids.popleft()
+                encoded_id3 = encoded_ids.popleft()
+                token_type_ids.popleft()
+                token_type_ids.popleft()
+                char = self.base_tokenizer.decode([encoded_id, encoded_id2, encoded_id3])[0]
+                char_splitted = list(split_syllables(char))[:3]
+                char_splitted = char_splitted + (3-len(char_splitted))*[self.base_tokenizer.pad_token]
+                cho, joong, jong = char_splitted
+                encoded_ids_new.extend([self.jamo_to_id[cho], self.jamo_to_id[joong], self.jamo_to_id[jong]])
+                token_type_ids_new.extend([1,2,3])
+        return encoded_ids_new, token_type_ids_new
+
+    def batch_encode_char(self, sentences):
+        input_ids = []
+        attention_mask = []
+        token_type_ids = []
+        for sentence in sentences:
+            input_ids_row, token_type_id = self.encode_char(sentence)
+            input_ids.append(input_ids_row)
+            token_type_ids.append(token_type_id)
+        max_length = max(list(map(len, input_ids)))
+        for i in range(len(sentences)):
+            input_ids[i] = input_ids[i] + (max_length-len(input_ids[i])) * [self.base_tokenizer.eos_token_id]
+            attention_mask.append([1 if input_id!=self.base_tokenizer.eos_token_id else 0 for input_id in input_ids[i]])
+            token_type_ids[i] = token_type_ids[i] + (max_length-len(token_type_ids[i])) * [0]
+        return (
+            torch.LongTensor(input_ids),
+            torch.LongTensor(attention_mask),
+            torch.LongTensor(token_type_ids)
+        )
+
+    def batch_encode_jamo_from_char_encoded(self, batch_encoded_ids, batch_token_type_ids):
+        input_ids = []
+        attention_mask = []
+        token_type_ids_new = []
+        for encoded_ids, token_type_ids in zip(batch_encoded_ids, batch_token_type_ids):
+            encoded_ids_row, token_type_ids_row = self.encode_jamo_from_char_encoded(encoded_ids, token_type_ids)
+            attention_mask.append([1 if encoded_id!=self.base_tokenizer.eos_token_id else 0 for encoded_id in encoded_ids_row])
+            input_ids.append(encoded_ids_row)
+            token_type_ids_new.append(token_type_ids_row)
+        
+        return (
+            torch.LongTensor(input_ids),
+            torch.LongTensor(attention_mask),
+            torch.LongTensor(token_type_ids_new),
+        )
+
+    def batch_encode_jamo(self, sentences):
+        input_ids = []
+        attention_mask = []
+        token_type_ids = []
+        for sentence in sentences:
+            input_ids_row, token_type_id = self.encode_jamo(sentence)
+            input_ids.append(input_ids_row)
+            token_type_ids.append(token_type_id)
+        max_length = max(list(map(len, input_ids)))
+        
+        for i in range(len(sentences)):
+            input_ids[i] = input_ids[i] + (max_length-len(input_ids[i])) * [self.base_tokenizer.eos_token_id]
+            attention_mask.append([1 if input_id!=self.base_tokenizer.eos_token_id else 0 for input_id in input_ids[i]])
+            token_type_ids[i] = token_type_ids[i] + (max_length-len(token_type_ids[i])) * [0]
+
+        return (
+            torch.LongTensor(input_ids),
+            torch.LongTensor(attention_mask),
+            torch.LongTensor(token_type_ids),
+        )
+    
+
+class SentenceTokenizer:
+    def __init__(
+        self,
+        min_length=32,
+        max_length=64,
+        n_overlap=3,
+        roll=False
+    ):
+        self.min_length = min_length
+        self.max_length = max_length
+        self.n_overlap = n_overlap
+        self.roll = roll
+
+    def split_text_into_sentences(self, text):
+        split_text = re.split(r'([^가-힣] )', text)
+        split_text = [split_text[i] + split_text[i + 1] for i in range(0, len(split_text) - 1, 2)] + ([split_text[-1]] if len(split_text) % 2 != 0 else [])
+
+        return split_text
+
+    def merge_chunks(self, chunks):
+        merged_chunks = []
+        buffer = ""
+
+        for chunk in chunks:
+            buffer += chunk
+            if len(buffer) > self.min_length:  # If buffer meets the min length, finalize it
+                merged_chunks.append(buffer)
+                buffer = ""
+
+        # Add any remaining buffer as the last chunk
+        if buffer:
+            merged_chunks.append(buffer)
+
+        return merged_chunks
+
+    def merge_chunks_reverse(self, chunks):
+        chunks_reverse = []
+        for chunk in chunks[::-1]:
+            chunks_reverse.append(chunk[::-1])
+            
+        merged_chunks = []
+        buffer = ""
+
+        for chunk in chunks_reverse:
+            buffer += chunk
+            if len(buffer) > self.min_length:  # If buffer meets the min length, finalize it
+                merged_chunks.append(buffer)
+                buffer = ""
+
+        # Add any remaining buffer as the last chunk
+        if buffer:
+            merged_chunks.append(buffer)
+
+        res_merged_chunks = []
+        for chunk in merged_chunks[::-1]:
+            res_merged_chunks.append(chunk[::-1])
+
+        return res_merged_chunks
+        
+    def split_text(self, text):
+        words = self.split_space(text)
+        
+        # Step 2: Greedily merge words until the length of the merged text is shorter than max_length
+        splitted_chunks = []
+        buffer = []
+        
+        for word in words:
+            buffer.append(word)  # Add the word to the buffer
+            merged_text = ''.join(buffer)
+            
+            # If the merged text exceeds max_length, push the current buffer to the result
+            if len(merged_text) > self.max_length:
+                # Remove the last added word and save the current buffer as a chunk
+                buffer.pop()
+                splitted_chunks.append(''.join(buffer))
+                buffer = [''+word]  # Start a new buffer with the current word
+    
+        # Step 3: Append the left over buffer
+        if buffer:
+            splitted_chunks.append(''.join(buffer))
+    
+        return splitted_chunks
+
+    def tokenize(self, text):
+        splitted_chunks = []
+        # Step 1: Split text into sentences
+        sentences = self.split_text_into_sentences(text)
+        for chunk in sentences:
+            if len(chunk)>=self.max_length:
+                splitted_chunks.extend(self.split_text(chunk))
+            else:
+                splitted_chunks.append(chunk)
+        merged_chunks = self.merge_chunks(splitted_chunks)
+        merged_chunks = self.merge_chunks_reverse(merged_chunks)
+
+        return merged_chunks
+
+    def split_space(self, text):
+        split_text = re.split(r'(\s+)', text)  # Keep spaces as part of the split parts
+        filtered_text = [s + sp for s, sp in zip(split_text[::2], split_text[1::2] + [''])] 
+        return filtered_text
+    
+    def overlap(self, chunks):
+        if not chunks:
+            return []
+        if self.roll:
+            chunks = [chunks[-1]] + chunks + [chunks[0]]
+        res = []
+        total_idx = 0
+        for chunk_idx in range(len(chunks)-1):
+            chunk_a, chunk_b = chunks[chunk_idx], chunks[chunk_idx+1]
+            chunk_a_words, chunk_b_words = self.split_space(chunk_a), self.split_space(chunk_b)
+            chunk_a_overlap_length, chunk_b_overlap_length = len(chunk_a_words)//self.n_overlap, len(chunk_b_words)//self.n_overlap
+            for overlap_idx in range(self.n_overlap):
+                chunk_a_past, chunk_a_overlap, chunk_b_overlap = ''.join(chunk_a_words[:chunk_a_overlap_length*overlap_idx]), ''.join(chunk_a_words[chunk_a_overlap_length*overlap_idx:]), ''.join(chunk_b_words[:chunk_b_overlap_length*overlap_idx])
+                overlap = chunk_a_overlap+chunk_b_overlap
+                start = total_idx+len(chunk_a_past)
+                end = start + len(overlap)
+                res.append((start, end, overlap))
+            total_idx += len(chunk_a)
+        res.append((total_idx, total_idx+len(chunks[-1]), chunks[-1]))
+
+        return res
+
+    def decode_overlap(self, chunks):
+        if not chunks:
+            return ""
+        
+        # Determine total length based on the largest end index
+        max_length = max(end for _, end, _ in chunks)
+        
+        # Dictionary to store characters at each index
+        index_char_map = {i: [] for i in range(max_length)}
+        
+        # Populate index_char_map with characters from chunks
+        for start, end, chunk in chunks:
+            for i, char in enumerate(chunk):
+                index = start + i
+                if index < max_length:
+                    index_char_map[index].append(char)
+        
+        # Reconstruct text using majority vote
+        reconstructed_text = []
+        for i in range(max_length):
+            most_common_char, _ = Counter(index_char_map[i]).most_common(1)[0]
+            reconstructed_text.append(most_common_char)
+        res = "".join(reconstructed_text)
+        if self.roll:
+            res = res[len(chunks[0][2]):-len(chunks[-1][2])]
+    
+        return res
