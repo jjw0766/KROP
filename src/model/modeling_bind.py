104558d5876ac8f5db6aa41b04f0ee200c2e9515
jwengr <jinwoo0766@gmail.com>
2025-08-16 14:10:56 +0900

    fix

--- src/model/modeling_bind.py
+++ src/model/modeling_bind.py
@@ -23,7 +23,15 @@ class BIND(nn.Module):
         self.base_model_config.sliding_window = sliding_window
         base_model = AutoModelForCausalLM.from_pretrained(base_model_name, config=self.base_model_config)
         if use_bntd:
-            base_model.model.forward = MethodType(model_forward, base_model.model)
+            base_model.model.sliding_window = sliding_window
+            if 'qwen3' in base_model_name.lower():
+                base_model.model.forward = MethodType(qwen3_forward, base_model.model)
+                print('use full attn qwen3')
+            elif 'gemma-3' in base_model_name.lower():
+                base_model.model.forward = MethodType(gemma3_forward, base_model.model)
+                print('use full attn gemma3')
+            else:
+                raise ValueError('full attn model not found.')
         self.model = base_model
 
     def set_tokenizer(self, bind_tokenizer: BINDTokenizer):
@@ -58,9 +66,95 @@ class BIND(nn.Module):
                 pred_ids = logits[idx][:-2].argmax(-1).detach().cpu().tolist()
                 sentence_denoised.append(self.tokenizer.decode_char(pred_ids, False))
         return loss, logits, pred_ids, sentence_denoised
- 
+
+def gemma3_forward(
+    self,
+    input_ids: Optional[torch.LongTensor] = None,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_values = None,
+    inputs_embeds: Optional[torch.FloatTensor] = None,
+    use_cache: Optional[bool] = None,
+    output_attentions: Optional[bool] = None,
+    output_hidden_states: Optional[bool] = None,
+    cache_position: Optional[torch.LongTensor] = None,
+    **flash_attn_kwargs,
+) -> BaseModelOutputWithPast:
+    output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+    output_hidden_states = (
+        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+    )
+
+    if inputs_embeds is None:
+        inputs_embeds = self.embed_tokens(input_ids)
+
+    if cache_position is None:
+        past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
+        cache_position = torch.arange(
+            past_seen_tokens,
+            past_seen_tokens + inputs_embeds.shape[1],
+            device=inputs_embeds.device,
+        )
+
+    if position_ids is None:
+        position_ids = cache_position.unsqueeze(0)
+
+    attention_mask = _prepare_4d_attention_mask(attention_mask, self.dtype)
     
-def model_forward(
+    # Create the masks
+    attention_mask_mapping = {
+        "full_attention": attention_mask,
+    }
+    sliding_attention_mask = torch.tril(torch.triu(attention_mask, diagonal=-self.sliding_window), diagonal=self.sliding_window)
+    attention_mask_mapping["sliding_attention"] = sliding_attention_mask
+
+    # embed positions
+    hidden_states = inputs_embeds
+
+    # create position embeddings to be shared across the decoder layers
+    position_embeddings_global = self.rotary_emb(hidden_states, position_ids)
+    position_embeddings_local = self.rotary_emb_local(hidden_states, position_ids)
+
+    # decoder layers
+    all_hidden_states = () if output_hidden_states else None
+    all_self_attns = () if output_attentions else None
+
+    for decoder_layer in self.layers[: self.config.num_hidden_layers]:
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        layer_outputs = decoder_layer(
+            hidden_states,
+            position_embeddings_global=position_embeddings_global,
+            position_embeddings_local=position_embeddings_local,
+            attention_mask=attention_mask_mapping[decoder_layer.attention_type],
+            position_ids=position_ids,
+            past_key_value=past_key_values,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            **flash_attn_kwargs,
+        )
+
+        hidden_states = layer_outputs[0]
+
+        if output_attentions:
+            all_self_attns += (layer_outputs[1],)
+
+    hidden_states = self.norm(hidden_states)
+
+    if output_hidden_states:
+        all_hidden_states += (hidden_states,)
+
+    return BaseModelOutputWithPast(
+        last_hidden_state=hidden_states,
+        past_key_values=past_key_values,
+        hidden_states=all_hidden_states,
+        attentions=all_self_attns,
+    )
+
+    
+def qwen3_forward(
     self,
     input_ids: Optional[torch.LongTensor] = None,
     attention_mask: Optional[torch.Tensor] = None,
@@ -71,8 +165,6 @@ def model_forward(
     cache_position: Optional[torch.LongTensor] = None,
     **kwargs ,
 ) -> BaseModelOutputWithPast:
-    if (input_ids is None) ^ (inputs_embeds is not None):
-        raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
     if inputs_embeds is None:
         inputs_embeds = self.embed_tokens(input_ids)
